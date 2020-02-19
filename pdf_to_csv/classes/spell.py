1a0ddd441bb0fdf59956f0e29a04d7690593681e
KhaosBlaze <charless@localhost.localdomain>
2020-02-19 15:58:10 -0500

    update spell class

---
+++ pdf_to_csv/classes/spell.py
@@ -0,0 +1,18 @@
+class Spell:
+    """Spell class for creating the large index to go into go to csv"""
+    def __init__(self):
+        self.name = None
+        self.level = None
+        self.school = None
+        self.casting_time =  None
+        self.range = None
+        self.components = None
+        self.Duration = None
+        self.Description = None
+        self.class = []
+
+    def add_class(self, class):
+        self.class.append(class)
+
+
+        
